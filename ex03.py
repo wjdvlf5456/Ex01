f38f418904d221bb105f46980377e65d659c95b0
최정필 <wjdvlf1407@gmail.com>
2022-07-13 12:07:10 +0900

    2,8,16진수 표기법

--- ex03.py
+++ ex03.py
@@ -9,3 +9,16 @@ print(b,type(b))
 #다음과 같이 작성하였을 때 print(a)는 2로 출력된다.
 a = True + True
 print(a,type(a))
+
+print(1 == True)
+print(0 == True)
+print(0 == False)
+
+#특이한 케이스
+b1 = True   # 1
+b2 = False  # 0
+
+print(b1 + 10)      #1+10 --> 11
+print(b2 + 10)      #0+10 --> 10
+print(True+True)    #1+1 --> 2
+print(True - 2)     #1-2 --> -1
