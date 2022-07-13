03825f3728e984dee392cc5ef9f2a505cd37eacc
최정필 <wjdvlf1407@gmail.com>
2022-07-13 11:40:11 +0900

    Initial commit

---
+++ ex01.py
@@ -0,0 +1,25 @@
+import keyword
+
+print("01")
+
+#변수명
+
+friend = 1
+a =  10
+my_name = "황일영"
+myName = '황일영'
+_yourName = "유재석"
+number1 = "박명수"
+
+print(friend)
+print(a)
+print(my_name)
+print(myName)
+print(_yourName)
+print(number1)
+
+a=3
+b=6
+
+a,b = b,a
+print(a,b)
