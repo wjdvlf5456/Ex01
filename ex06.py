8301def087a4bc4a4cad7a85dd7524d6f8b86176
최정필 <wjdvlf1407@gmail.com>
2022-07-14 11:03:31 +0900

    ex07,ex08 연산자

--- ex06.py
+++ ex06.py
@@ -9,3 +9,10 @@ b = 7-2j
 print(a+b)
 
 print(b.real)
+print(b.imag)
+
+c=4
+d=5.4
+
+print(c+d)
+print((c+d).real)
---
+++ ex08.py
@@ -0,0 +1,32 @@
+#확장 연산자
+x=2
+x=x*5
+print(x)
+
+x-=3
+print(x)
+
+x**=3
+print(x)
+
+print("==============================")
+
+#관계 연산자
+print(1>3)
+print(2<4)
+print(4<=5)
+print(4>=5)
+print(6==9)
+print(6!=9)
+
+print("==============================")
+
+a=10
+b=20
+c=a
+print(a==b)
+print(a is b)
+print(a is c)
+
+
+
