c17572f5d97fd9704b14d852dd0b7be71affb4bd
최정필 <wjdvlf1407@gmail.com>
2022-07-14 12:18:43 +0900

    문자열 :,배열

--- ex10.py
+++ ex10.py
@@ -1,6 +1,25 @@
+from operator import le
+
 str1 = "FirstString"
 str2 = "SecondString"
 
 print(str1 + str2)  #print(str1 + 10) --> 문자열만 가능
 print(str1 * 3)
 print(str1 + str(10))
+print(str1[5])
+print(str1[-1])
+print(str1[2:5])    #[이상:미만]
+print(str1[1:9:3])  #[이상:미만:증가]
+print(str1[:9])     #[:미만]
+print(len(str1))    #len(str) --> 문자열의 길이
+
+print(str1[:])
+print(str1[::3])    #Fstn으로 출력됨
+print(str1[-3])
+
+name = "우영우"
+name = "우영후"
+name2 = name[:-1:]
+
+print(name)     #우영후
+print(name2)    #우영
