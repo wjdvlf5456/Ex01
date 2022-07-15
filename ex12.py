f19f4e959bb197266c0c8bf03c914d1a8d9f7848
최정필 <wjdvlf1407@gmail.com>
2022-07-15 10:41:07 +0900

    ex12 문자열 대문자 소문자 변환

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#upper,lower
+
+s="i like Python"
+print(s.upper())        #모든 글자를 대문자로 변환
+print(s.lower())        #모든 글자를 소문자로 변환
+print(s.swapcase())     #소문자는 대문자, 대문자는 소문자로 변환
+print(s.capitalize())   #첫 글자만 대문자
